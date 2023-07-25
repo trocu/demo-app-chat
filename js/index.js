75037110e704bc2a2604f5e5eb122729af14fd03
Mariusz Trocewicz <throcu@gmail.com>
2023-07-26 01:35:18 +0200

    add: body-parser, socket.io, fetchAPI, event listeners

--- js/index.js
+++ js/index.js
@@ -1,15 +1,69 @@
-// const bootstrap = require('bootstrap');
+const conversation = document.querySelector('#messages');
+const button = document.querySelector('#send');
+// const nameInput = document.querySelector('#alias');
+// const messageInput = document.querySelector('#message');
+const form = document.querySelector('#form');
 
-// const addMessage = message => {
-//   const heading = document.createElement('h4');
-//   heading.textContent = message.name;
-//   const text = document.createElement('p');
-//   text.textContent = message.message;
-//   document.querySelector('#message').append(heading, text);
-// };
-const addMessage = message => {
-  const msg = document.querySelector('#message');
-  msg.insertAdjacentHTML('beforeend', `<h4>${message.name}</h4> <p>${message.message}</p>`);
+// form.addEventListener('input', e => {
+//   console.log(message.value);
+//   console.log(alias.value);
+// });
+
+window.onload = () => {
+  fetchMessage().then(message => renderMessage(message));
 };
 
-addMessage({ name: 'Tim', message: 'hello' });
+button.addEventListener('click', () => {
+  const {
+    elements: { name, message },
+  } = form;
+
+  const messages = { name: `${name.value}`, message: `${message.value}` };
+  // console.log(form.elements.name.value);
+  // console.log(form.elements.message.value);
+  postMessage(messages);
+  fetchMessage().then(message => renderMessage(message));
+});
+
+// const addMessage = message => {
+//   message.insertAdjacentHTML('beforeend', `<h4>${message.name}</h4> <p>${message.message}</p>`);
+// };
+
+const fetchMessage = () => {
+  return fetch('http://localhost:3000/messages')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => console.log(error));
+};
+
+const postMessage = message => {
+  return fetch('http://localhost:3000/messages', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(message),
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return;
+    })
+    .catch(error => console.log(error));
+};
+
+const renderMessage = messages => {
+  const markup = messages
+    .map(message => {
+      return `<h4>${message.name}</h4> <p>${message.message}</p>`;
+    })
+    .join('');
+  conversation.innerHTML = markup;
+  // conversation.insertAdjacentHTML('beforeend', markup);
+};
+
+const socket = io();
+// socket.on('message', fetchMessage());
--- js/server.js
+++ js/server.js
@@ -1,16 +1,45 @@
 const path = require('path');
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 const port = 3000;
 const rootPath = path.join(__dirname, '..');
 // const publicPath = path.join(rootPath, 'js');
 
-app.use(express.static(rootPath));
-const server = app.listen(port, () => {
+const server = http.listen(port, () => {
   console.log('Server is listening on port', server.address().port);
 });
 
-// app.get('./index.js', (req, res) => {
-//   res.set('Content-Type', 'application/javascript');
-//   res.sendFile('http://localhost:3000/js/index.js');
+// io.on('connection', socket => {
+//   console.log('User connected');
+//   socket.on('message', data => {
+//     console.log('Message received', data);
+//   });
 // });
+
+io.on('connection', () => {
+  console.log('User connected');
+});
+
+// server.listen(port);
+// console.log('Server is listening on port', server.address().port);
+
+const messages = [
+  { name: 'Tim', message: 'hello' },
+  { name: 'Jane', message: 'hi' },
+];
+
+app.use(express.static(rootPath));
+app.use(bodyParser.json());
+
+app.get('/messages', (req, res) => {
+  res.send(messages);
+});
+
+app.post('/messages', (req, res) => {
+  messages.push(req.body);
+  io.emit('message', req.body);
+  res.sendStatus(200);
+});
